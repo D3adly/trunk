ea199cb71b8fc08e8d2524c0d23524e8696cc69e
auris <aur.gaizauskas@gmail.com>
2014-04-06 16:23:45 +0100

    Basic structure

---
+++ library/Core.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: auris
+ * Date: 06/04/14
+ * Time: 16:20
+ */
+class Core
+{
+	private $controller;
+	private $action;
+	private $view;
+
+	public function __construct()
+	{
+
+	}
+
+	public function loadPage()
+	{
+
+	}
+} 
